70ace19a7a964dff72f9c87d56f46b0f5bfa0b91
quadrant26 <quadrant26@163.com>
2017-07-12 18:00:45 +0800

    星评分组件

--- angular-app/src/app/stars/stars.component.ts
+++ angular-app/src/app/stars/stars.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-stars',
@@ -6,10 +6,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./stars.component.css']
 })
 export class StarsComponent implements OnInit {
-
-  constructor() { }
-
+  @Input()
+  public rating: number;
+  public stars = [];
+  constructor() {
+  }
   ngOnInit() {
+    const full: number = Math.floor(this.rating);
+    const half: number = Math.ceil(this.rating - full);
+    const empty: number = 5 - full - half;
+    for (let i = 0; i < 5; i++) {
+      if (i < full) {
+        this.stars.push('full');
+      } else if (i === full && half !== 0) {
+        this.stars.push('half')
+      } else {
+        this.stars.push('empty')
+      }
+    }
   }
 
 }
+
