99ff81e8c1b9fd7043808fcabf04122fffac6175
quadrant26 <quadrant26@163.com>
2017-07-12 18:00:24 +0800

    商品详情组件

--- angular-app/src/app/product/product.component.ts
+++ angular-app/src/app/product/product.component.ts
@@ -7,9 +7,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductComponent implements OnInit {
 
+  public products: Array<Product>;
   constructor() { }
 
   ngOnInit() {
+    this.products = [
+      new Product(1, '第一个商品', 899, 3.5, '这是一个垃圾电脑', ['电子产品', '家电']),
+      new Product(2, '第二个商品', 899, 3.5, '这是一个垃圾电脑', ['电子产品', '家电']),
+      new Product(3, '第三个商品', 899, 3.5, '这是一个垃圾电脑', ['电子产品', '家电']),
+      new Product(4, '第四个商品', 899, 3.5, '这是一个垃圾电脑', ['电子产品', '家电']),
+      new Product(5, '第五个商品', 899, 3.5, '这是一个垃圾电脑', ['电子产品', '家电']),
+      new Product(6, '第六个商品', 899, 3.5, '这是一个垃圾电脑', ['电子产品', '家电'])
+    ]
   }
 
+}
+
+export class Product{
+  constructor(
+    public id: number,
+    public title: string,
+    public price: number,
+    public rating: number,
+    public desc: string,
+    public categories: Array<string>
+  ){
+
+  }
 }
