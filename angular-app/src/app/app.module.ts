056fbbad0d1f1b56720cb4c679141b24d0cffbd2
quadrant26 <quadrant26@163.com>
2017-07-12 14:11:56 +0800

    模块导入文件

--- angular-app/src/app/app.module.ts
+++ angular-app/src/app/app.module.ts
@@ -2,10 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { SearchComponent } from './search/search.component';
+import { ProductComponent } from './product/product.component';
+import { StarsComponent } from './stars/stars.component';
+import { FootComponent } from './foot/foot.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    SearchComponent,
+    ProductComponent,
+    StarsComponent,
+    FootComponent
   ],
   imports: [
     BrowserModule
