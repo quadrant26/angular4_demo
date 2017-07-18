79429aa7f4a061055f88b95aa24f50fd822a4513
quadrant26 <quadrant26@163.com>
2017-07-18 11:45:17 +0800

    router config

---
+++ angular-app/src/app/app.router.ts
@@ -0,0 +1,20 @@
+import { NgModule } from "@angular/core";
+import { RouterModule, Routes} from "@angular/router";
+import { HomeComponent } from "./home/home.component";
+import { ProductDetailComponent } from "./product-detail/product-detail.component";
+import { Code404Component } from "./code404/code404.component";
+
+const routes: Routes = [
+  { path : "", component : HomeComponent},
+  { path : "product/:prodTitle", component : ProductDetailComponent},
+  { path : "**", component : Code404Component}
+];
+
+@NgModule({
+  imports : [RouterModule.forRoot(routes)],
+  exports : [RouterModule],
+  providers : []
+})
+
+export class AppRoutingModule {
+}
