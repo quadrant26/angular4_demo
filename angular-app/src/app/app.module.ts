1dd37721232e114f625f1dd03ced7ffa01a6b469
quadrant26 <quadrant26@163.com>
2017-07-18 11:45:03 +0800

    add home and productDetail component

--- angular-app/src/app/app.module.ts
+++ angular-app/src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpModule } from "@angular/http";
+import { AppRoutingModule } from "./app.router";
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
@@ -8,6 +10,9 @@ import { ProductComponent } from './product/product.component';
 import { StarsComponent } from './stars/stars.component';
 import { FooterComponent } from './footer/footer.component';
 import { CarouselComponent } from './carousel/carousel.component';
+import { HomeComponent } from './home/home.component';
+import { Code404Component } from './code404/code404.component';
+import { ProductDetailComponent } from './product-detail/product-detail.component';
 
 @NgModule({
   declarations: [
@@ -17,10 +22,15 @@ import { CarouselComponent } from './carousel/carousel.component';
     ProductComponent,
     StarsComponent,
     CarouselComponent,
-    FooterComponent
+    FooterComponent,
+    HomeComponent,
+    Code404Component,
+    ProductDetailComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
