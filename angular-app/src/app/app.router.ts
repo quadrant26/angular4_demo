04a83b20f8193eb569eb932d741d60a5a1195dd5
quadrant26 <quadrant26@163.com>
2017-07-19 18:19:44 +0800

    router argument => productId

--- angular-app/src/app/app.router.ts
+++ angular-app/src/app/app.router.ts
@@ -6,7 +6,7 @@ import { Code404Component } from "./code404/code404.component";
 
 const routes: Routes = [
   { path : "", component : HomeComponent},
-  { path : "product/:prodTitle", component : ProductDetailComponent},
+  { path : "product/:productId", component : ProductDetailComponent},
   { path : "**", component : Code404Component}
 ];
 
