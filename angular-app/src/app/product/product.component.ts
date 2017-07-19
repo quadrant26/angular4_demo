54512dbca4af8300432f4ba9051c476380136bce
quadrant26 <quadrant26@163.com>
2017-07-19 18:22:27 +0800

    依赖注入 productService 获取 products

--- angular-app/src/app/product/product.component.ts
+++ angular-app/src/app/product/product.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {ProductService} from "../shared/product.service";
 
 @Component({
   selector: 'app-product',
@@ -7,18 +8,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductComponent implements OnInit {
 
-  public products: Array<Product>;
-  constructor() { }
+  //public products: Array<Product>;
+  public products: Product[];
+
+  constructor(private productService: ProductService) { }
 
   ngOnInit() {
-    this.products = [
-      new Product(1, '第一个商品', 899, 3.5, '这是一个垃圾电脑', ['电子产品', '家电']),
-      new Product(2, '第二个商品', 899, 3.5, '这是一个垃圾电脑', ['电子产品', '家电']),
-      new Product(3, '第三个商品', 899, 3.5, '这是一个垃圾电脑', ['电子产品', '家电']),
-      new Product(4, '第四个商品', 899, 3.5, '这是一个垃圾电脑', ['电子产品', '家电']),
-      new Product(5, '第五个商品', 899, 3.5, '这是一个垃圾电脑', ['电子产品', '家电']),
-      new Product(6, '第六个商品', 899, 3.5, '这是一个垃圾电脑', ['电子产品', '家电'])
-    ]
+    // this.products = [
+    //   new Product(1, '第一个商品', 899, 3.5, '这是一个垃圾电脑', ['电子产品', '家电']),
+    //   new Product(2, '第二个商品', 899, 3.5, '这是一个垃圾电脑', ['电子产品', '家电']),
+    //   new Product(3, '第三个商品', 899, 3.5, '这是一个垃圾电脑', ['电子产品', '家电']),
+    //   new Product(4, '第四个商品', 899, 3.5, '这是一个垃圾电脑', ['电子产品', '家电']),
+    //   new Product(5, '第五个商品', 899, 3.5, '这是一个垃圾电脑', ['电子产品', '家电']),
+    //   new Product(6, '第六个商品', 899, 3.5, '这是一个垃圾电脑', ['电子产品', '家电'])
+    // ]
+
+    this.products = this.productService.getProducts();
   }
 
 }
