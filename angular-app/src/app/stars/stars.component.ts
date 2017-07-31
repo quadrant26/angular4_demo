fefd9f3db063eeb774a533415c44f9af08fcea95
quadrant26 <quadrant26@163.com>
2017-07-31 14:45:27 +0800

    点击评分 stars

--- angular-app/src/app/stars/stars.component.ts
+++ angular-app/src/app/stars/stars.component.ts
@@ -1,30 +1,56 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {Component, OnInit, Input, Output, OnChanges, EventEmitter, SimpleChanges} from '@angular/core';
 
 @Component({
   selector: 'app-stars',
   templateUrl: './stars.component.html',
   styleUrls: ['./stars.component.css']
 })
-export class StarsComponent implements OnInit {
+export class StarsComponent implements OnInit, OnChanges {
   @Input()
-  public rating: number;
-  public stars = [];
+  private rating: number = 0;
+  private stars: boolean[];
+
+  @Input()
+  private readonly: boolean = true;
+
+  @Output()
+  private ratingChange: EventEmitter<number> = new EventEmitter();
+
+
   constructor() {
   }
+
   ngOnInit() {
-    const full: number = Math.floor(this.rating);
-    const half: number = Math.ceil(this.rating - full);
-    const empty: number = 5 - full - half;
-    for (let i = 0; i < 5; i++) {
-      if (i < full) {
-        this.stars.push('full');
-      } else if (i === full && half !== 0) {
-        this.stars.push('half')
-      } else {
-        this.stars.push('empty')
-      }
+    // this.stars = [];
+    // const full: number = Math.floor(this.rating);
+    // const half: number = Math.ceil(this.rating - full);
+    // const empty: number = 5 - full - half;
+    // for (let i = 0; i < 5; i++) {
+    //   if (i < full) {
+    //     this.stars.push('full');
+    //   } else if (i === full && half !== 0) {
+    //     this.stars.push('half')
+    //   } else {
+    //     this.stars.push('empty')
+    //   }
+    // }
+  }
+
+  clickStar(index: number) {
+    if (!this.readonly){
+      this.rating = index + 1;
+      this.ratingChange.emit(this.rating);
+      //this.ngOnInit()
     }
   }
+
+  ngOnChanges(changes: SimpleChanges): void{
+    this.stars = [];
+    for (let i = 1; i <= 5; i++) {
+      this.stars.push(i > this.rating ? true : false);
+    }
+  }
+
 
 }
 
